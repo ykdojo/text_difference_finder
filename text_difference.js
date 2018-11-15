84e364f05fe4faf5ade425a216cebbdfa1ba8c6d
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2018-11-15 17:59:29 -0500

    fixed naming issues

--- text_difference.js
+++ text_difference.js
@@ -1,20 +1,20 @@
-function textDiff(s1, s2) {
+function textDiff(s1, s2, splitByChar) {
   let memo = [...Array(s1.length)].map(e => Array(s2.length));
-  let tdResult = TD(s1, s2, 0, 0, memo);
+  let tdResult = LCS(s1, s2, 0, 0, memo);
 
   let s1Result = [];
   let s2Result = [];
   let i1 = 0;
   let i2 = 0;
   for (i = 0; i < tdResult.length; i++) {
-    let commonChar = tdResult[i];
+    let commonStr = tdResult[i];
 
-    i1Next = s1.indexOf(commonChar, i1);
-    pushResult(s1Result, s1, i1, i1Next, 'deleted', commonChar);
+    i1Next = s1.indexOf(commonStr, i1);
+    pushResult(s1Result, s1, i1, i1Next, 'deleted', commonStr);
     i1 = i1Next + 1;
 
-    i2Next = s2.indexOf(commonChar, i2);
-    pushResult(s2Result, s2, i2, i2Next, 'added', commonChar);
+    i2Next = s2.indexOf(commonStr, i2);
+    pushResult(s2Result, s2, i2, i2Next, 'added', commonStr);
     i2 = i2Next + 1;
   }
 
@@ -24,12 +24,12 @@ function textDiff(s1, s2) {
   i2Next = s2.length;
   pushResult(s2Result, s2, i2, i2Next, 'added', '');
 
-  return {lca: tdResult,
+  return {lcs: tdResult,
           s1Result: s1Result.join(''),
           s2Result: s2Result.join('')};
 }
 
-function TD(s1, s2, i1, i2, memo) {
+function LCS(s1, s2, i1, i2, memo) {
   if (s1.length == i1 || s2.length == i2 ) {
     return '';
   }
@@ -39,13 +39,13 @@ function TD(s1, s2, i1, i2, memo) {
   }
 
   if (s1[i1] == s2[i2]) {
-    memo[i1][i2] = s1[i1] + TD(s1, s2, i1 + 1, i2 + 1, memo);
+    memo[i1][i2] = s1[i1] + LCS(s1, s2, i1 + 1, i2 + 1, memo);
     return memo[i1][i2];
   }
 
   let result;
-  let resultA = TD(s1, s2, i1 + 1, i2, memo);
-  let resultB = TD(s1, s2, i1, i2 + 1, memo);
+  let resultA = LCS(s1, s2, i1 + 1, i2, memo);
+  let resultB = LCS(s1, s2, i1, i2 + 1, memo);
   if(resultA.length >= resultB.length) {
     result = resultA;
   } else {
@@ -56,10 +56,10 @@ function TD(s1, s2, i1, i2, memo) {
 }
 
 function pushResult(resultArr, strToCompare, index,
-                    indexNext, className, commonChar) {
+                    indexNext, className, commonStr) {
   if (indexNext > index) {
     let changed = strToCompare.substring(index, indexNext);
     resultArr.push(`<span class='${className}'>${changed}</span>`);
   }
-  resultArr.push(commonChar);
+  resultArr.push(commonStr);
 }
